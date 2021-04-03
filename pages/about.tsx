1dca94198f5cf036c1f5633693b3df8b66389a9c
John Dennehy <john.dennehy@avadolearning.com>
2021-04-03 16:24:18 +0100

    Adds initial Page structure and converts existing files to TSX and SCSS

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div className="container">
+      <Head>
+        <title>John Dennehy - About me</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>About Page</h1>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>John Dennehy - Portfolio!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hi, I'm JD!</h1>
+    </div>
+  )
+}
