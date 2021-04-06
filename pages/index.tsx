fb01e294da32a31fe45d6a1c5d3314634d22afff
John Dennehy <john.dennehy@avadolearning.com>
2021-04-06 20:12:26 +0100

    Feat - adds page transitions to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <div className="container">
+    <motion.div exit={{ opacity: 0 }} className="container">
       <Head>
         <title>John Dennehy - Portfolio!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hi, I'm JD!</h1>
-    </div>
+    </motion.div>
   )
 }
