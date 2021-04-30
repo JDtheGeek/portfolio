e33afc9d39c51d1bc126ef47563e63c069555b74
John Dennehy <john.dennehy@avadolearning.com>
2021-04-30 14:07:08 +0100

    Feat - adds CMS config and initial graphQL request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,47 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { request } from '../lib/datocms'
+
 import { motion } from 'framer-motion'
 import styles from '../styles/Home.module.scss'
 
-export default function Home() {
+const HOMEPAGE_QUERY = `query HomePage($limit: IntType) {
+  allProjects(first: $limit, filter: { showcase: {eq: true}}, orderBy: position_ASC) {
+    id
+    title
+    slug
+  }
+}`;
+
+export async function getStaticProps(context) {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: { limit: 10 },
+    preview: context.preview
+  });
+  return {
+    props: { data }
+  };
+}
+
+export default function Home({data}) {
   return (
     <motion.div exit={{ opacity: 0 }} className="container">
       <Head>
         <title>John Dennehy - Portfolio!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.outline}>
-        John <span className={styles.fill}>Dennehy</span>
-      </h1>
+      <main className={styles.container}>
+        <h1>
+          John <span className="dennehy">Dennehy</span>
+        </h1>
+      </main>
+      <div className="projects">
+        {JSON.stringify(data, null, 2)}
       <Link href="/projects">
-        <a>See projects</a>
+        <a>See all projects</a>
       </Link>
+      </div>
     </motion.div>
   )
 }
