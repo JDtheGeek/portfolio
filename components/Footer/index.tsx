678fd48b43b13304f0b04899dc1ee451cae5ca7f
John Dennehy <john.dennehy@avadolearning.com>
2021-05-09 18:29:22 +0100

    Feat- adds Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss'
+export const Footer = (): JSX.Element => {
+
+  return (
+    <footer className={styles.footer}>
+      <p>@JDtheGeek</p>
+    </footer>
+  )
+}
+
+export default Footer
