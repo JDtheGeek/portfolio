95ac2cf2402c18cfbae3177c129b4416c8962d48
John Dennehy <john.dennehy@avadolearning.com>
2021-05-09 18:37:55 +0100

    Fix- Amends datocms.ts to better handle requests without variables

--- lib/datocms.ts
+++ lib/datocms.ts
@@ -3,11 +3,11 @@ import { GraphQLClient } from "graphql-request";
 
 interface RequestProps {
   query: string
-  variables?: any
+  variables?: unknown
   preview?: boolean
 }
 
-export function request({ query, variables, preview }: RequestProps) {
+export function request({ query, variables, preview }: RequestProps): Promise<unknown> {
 
 const endpoint = preview
     ? `https://graphql.datocms.com/preview`
@@ -15,9 +15,10 @@ const endpoint = preview
 
   const client = new GraphQLClient(endpoint, {
     headers: {
-      authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`
+      'Content-Type': 'application/json',
+      'authorization': `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`
     }
   })
 
-  return client.request(query, variables)
+  return client?.request(query, variables)
 }
